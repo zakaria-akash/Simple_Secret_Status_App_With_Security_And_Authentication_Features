c1be2e59d5c773da0f1b87f75c33ec4d3448bc54
zakaria-akash <zakaria.93@yahoo.com>
2022-05-16 00:55:49 +0600

    Level 3 Authentication: Hashing the users' password with md5 package

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");  //belongs to Level 2 Authentication: database encryption
+const md5 = require("md5");
 
 const app = express();
 
@@ -23,9 +24,11 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
+//Level 2 Authentication: enable database encryption with mongoose-encryption package
 // const secret = "Thisisourlittlesecrets!";
 //using the environment variables from a .env file with dotenv package to protect our private information
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+//belongs to Level 2 Authentication: database encryption
 
 const User = new mongoose.model("User", userSchema);
 
@@ -45,7 +48,8 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    //Level 3 Authentication: enable hashing the password with md5 package
+    password: md5(req.body.password)
   });
   newUser.save((err) => {
     if (!err) {
@@ -58,7 +62,8 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  //Level 3 Authentication: enable hashing the password with md5 package
+  const password = md5(req.body.password);
   User.findOne({
     email: username
   }, (err, foundUser) => {
